36625af65f70665cea3de71269a995fa6dd824e6
telkomdev-rizky_n.a <kikiamaia1994@gmail.com>
2024-08-04 11:54:23 +0800

    add FE URL

--- src/app/index.js
+++ src/app/index.js
@@ -7,7 +7,7 @@ const basicAuth = require('../app/middleware/basicAuth');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.WEB_URL || 'http://localhost:3000',
   credentials: true
 }));
 
